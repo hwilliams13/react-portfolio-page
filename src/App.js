faa2aba03a0aa728367b3ad2d86a2e942aa5032c
harold <ill.will67@gmail.com>
2019-12-10 12:02:06 -0500

    added my favicon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -20,6 +22,13 @@ function App() {
         </a>
       </header>
     </div>
+    // <Router>
+    //   <Switch>
+    //     <Route>
+
+    //     </Route>
+    //   </Switch>
+    // </Router>
   );
 }
 
